dda7246323680d447200ada1ecd7138b45458a57
strengthtoendure <francisco.bahamondes90@gmail.com>
2018-06-21 01:52:40 -0400

    primer commit

---
+++ EjemploLibro/EjemploLibro/InputWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EjemploLibro
+{
+    class InputWrapper
+    {
+    }
+
+    internal struct AllInputButtons
+    {
+
+    }
+}
